3bea8ea8533de83c0c4253e9b1f35499fbc5afdd
qudcks1940 <94298697+qudcks1940@users.noreply.github.com>
2024-08-16 12:51:37 +0900

    [Silver IV] Title: 스택, Time: 136 ms, Memory: 38820 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,30 @@
+import sys
+from typing import Any
+
+input = sys.stdin.readline
+
+N = int(input().strip())
+
+stack=[]
+for i in range(N):
+  command = input().strip().split()
+
+  if command[0]=='push':
+    stack.append(command[1])
+  elif command[0]=='pop':
+    if len(stack)==0:
+      print(-1)
+    else:
+      print(stack.pop())
+  elif command[0]=='size':
+    print(len(stack))
+  elif command[0] =='empty':
+    if len(stack)==0:
+      print(1)
+    else:
+      print(0)
+  elif command[0]=='top':
+    if len(stack)==0:
+      print(-1)
+    else:
+      print(stack[-1])
