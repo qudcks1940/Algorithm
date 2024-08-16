124313e62097cf100e52c62db5b254ca23823201
qudcks1940 <94298697+qudcks1940@users.noreply.github.com>
2024-08-16 18:07:42 +0900

    [Silver IV] Title: 큐 2, Time: 2860 ms, Memory: 179356 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,36 @@
+import sys
+import queue
+from collections import deque
+input=sys.stdin.readline
+
+N=int(input().strip())
+
+myqueue = queue.Queue()
+
+for i in range(N):
+  command=input().strip().split()
+
+  if command[0]=='push':
+    myqueue.put(command[1])
+  elif command[0]=='pop':
+    if myqueue.qsize()==0:
+      print(-1)
+    else:
+      print(myqueue.get())
+  elif command[0]=='size':
+    print(myqueue.qsize())
+  elif command[0]=='empty':
+    if myqueue.qsize()>=1:
+      print(0)
+    else:
+      print(1)
+  elif command[0]=='front':
+    if myqueue.qsize()==0:
+      print(-1)
+    else:
+      print(myqueue.queue[0])
+  elif command[0]=='back':
+    if myqueue.qsize()==0:
+      print(-1)
+    else:
+      print(myqueue.queue[-1])
