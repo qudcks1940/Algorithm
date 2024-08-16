a430756c7b9f3b69f4db0c996d06f906be1bf413
qudcks1940 <94298697+qudcks1940@users.noreply.github.com>
2024-08-16 20:42:34 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,21 +1,18 @@
-# 재현이가 잘못된 수를 부를때마다 0을 외쳐서,
-# 재민이가 가장 최근에 쓴 수를 지우게 시킴.
-# 모든 수를 받아 적은 후 그 수의 합
-
 import sys
 
 input = sys.stdin.readline
 
+# 입력 받을 숫자의 개수
 K = int(input().strip())
 
-# numbers = [int(input().strip()) for _ in range(K)]
+stack = []
 
-numbers=[]
-for i in range(K):
-  number = int(input().strip())
-  if number==0:
-    numbers.pop()
-    continue
-  numbers.append(number)
-print(sum(numbers)) 
-  
+for _ in range(K):
+    num = int(input().strip())
+    if num == 0:
+        stack.pop()  # 최근 입력된 숫자를 제거
+    else:
+        stack.append(num)  # 숫자를 스택에 추가
+
+# 스택에 남아 있는 모든 수의 합 계산
+print(sum(stack))
