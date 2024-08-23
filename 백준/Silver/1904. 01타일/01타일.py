f5eef5438609872f18c0008db745522ebad9dc12
qudcks1940 <94298697+qudcks1940@users.noreply.github.com>
2024-08-23 20:55:09 +0900

    [Silver III] Title: 01타일, Time: 316 ms, Memory: 69652 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,14 @@
+# 백준 1904 01타일
+import sys
+
+input=sys.stdin.readline
+
+N=int(input().strip())
+
+dp=[0]*(1000001)
+
+dp[1]=1
+dp[2]=2
+for i in range(3,N+1):
+  dp[i]=(dp[i-2]+dp[i-1])%15746
+print(dp[N])
