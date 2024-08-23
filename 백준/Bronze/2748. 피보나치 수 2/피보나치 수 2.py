3fac6b8096c5322f22df12187832a6aeb7b658d1
qudcks1940 <94298697+qudcks1940@users.noreply.github.com>
2024-08-23 22:58:53 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,16 +1,16 @@
-# 백준 2748 피보나치 수 2
-
 import sys
 
 input=sys.stdin.readline
 
-dp=[0]*91
-
 N=int(input().strip())
-dp[0]=0
-dp[1]=1
 
-for i in range(2,N+1):
-  dp[i]=dp[i-1]+dp[i-2]
-
-print(dp[N])
+def fibo(n,memo={}):
+    if n in memo:
+        return memo[n]
+    if n==0:
+        return 0
+    if n<=2:
+        return 1
+    memo[n]=fibo(n-1)+fibo(n-2)
+    return memo[n]
+print(fibo(N))
