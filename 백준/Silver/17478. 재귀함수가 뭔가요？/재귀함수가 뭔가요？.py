b5c77e54aeebef1f850e6e3abe4b25b7cdab0b99
qudcks1940 <94298697+qudcks1940@users.noreply.github.com>
2025-01-01 16:06:41 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
@@ -0,0 +1,32 @@
+import sys
+
+input=sys.stdin.readline
+
+N=int(input().strip())
+
+sentence1="어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다."
+sentence2='"재귀함수가 뭔가요?"'
+sentence3='"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'
+sentence4='마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.'
+sentence5='그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."'
+
+sentence6='"재귀함수는 자기 자신을 호출하는 함수라네"'
+sentence7="라고 답변하였지."
+
+add="____"
+
+print(sentence1)
+
+for i in range(N):
+  print((add*i) + sentence2)
+  print((add*i) + sentence3)
+  print((add*i) + sentence4)
+  print((add*i) + sentence5)
+
+print(N*add + sentence2)
+print(N*add + sentence6)
+
+for j in range(N,-1,-1):
+  print(j*add + sentence7)
+
+
