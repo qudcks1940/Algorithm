cad0f09f9418f5809dbf7aa6352d00b29bbe3a18
qudcks1940 <94298697+qudcks1940@users.noreply.github.com>
2025-01-03 21:08:40 +0900

    [Bronze II] Title: 블랙잭, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,21 +1,29 @@
+# 백 준 2798
+# 블랙잭
+
 import sys
 import itertools
 
-input = sys.stdin.readline
+N, M=list(map(int,input().strip().split()))
 
-N, M = map(int,input().strip().split())
+numbers=list(map(int,input().strip().split()))
 
-cards = list(map(int,input().strip().split()))
+three=itertools.combinations(numbers,3)
 
-three_cards = itertools.combinations(cards, 3)
+isCheck=False
+max=0
 
-min_price = sys.maxsize
-right_card = []
-for card in three_cards:
-    if sum(card) <= M:
-        right_card.append(card)
-        sub=M-sum(card)
-        if sub < min_price:
-            min_price = sub  
-answer = M - min_price         
-print(answer)
+for i in three:
+  sum=0
+  for j in i:
+    sum+=j
+  if sum==M:
+    isCheck=True
+    print(M)
+    break
+  elif max<M and sum>max and sum<M:
+    max=sum
+
+if isCheck==False:
+  print(max)
+  
